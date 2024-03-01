16c53d6c315c8f331d64af8f2657966194b9ffc5
Francesca Bonato <francesca.bonato@hotmail.com>
2024-03-01 14:43:50 +0100

    add es02 completed - rename es01

---
+++ es02_cercare_elementi_nel_DOM/script.js
@@ -0,0 +1,11 @@
+// Cerca tutti gli elementi con classe "paragrafo".
+const paragraphs = document.querySelectorAll('.paragrafo');
+console.log(paragraphs)
+
+// Cerca l'elemento con id "elemento-3".
+const el3 = document.querySelectorAll('#elemento-3');
+console.log(el3);
+
+// Cerca tutti gli elementi h2 della pagina.
+const h2 = document.querySelectorAll('h2');
+console.log(h2);
