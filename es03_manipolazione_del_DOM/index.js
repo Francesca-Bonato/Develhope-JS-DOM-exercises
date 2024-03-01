8ce28cea26b1e8d9e9c83f80caa53e448d72d845
Francesca Bonato <francesca.bonato@hotmail.com>
2024-03-01 16:30:18 +0100

    update es03 with checkbox part

--- es03_manipolazione_del_DOM/index.js
+++ es03_manipolazione_del_DOM/index.js
@@ -1,7 +1,12 @@
 const addProduct = () => {
   const ul = document.querySelector('#my-todo-list');
-  const li = document.createElement('li');
   const input = document.querySelector('[name="my-input"]')
+
+  const li = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+
   li.innerText = input.value;
+  li.append(checkbox);
   ul.appendChild(li);
 };
